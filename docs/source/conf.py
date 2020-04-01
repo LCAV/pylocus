78db89636cab345cab8d61adc3accd95899e2738
duembgen <frederike.duembgen@epfl.ch>
2020-04-01 11:01:16 +0200

    Add .readthedocs.yml.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,6 @@ import os
 
 # -- General configuration ------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '2.0'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
